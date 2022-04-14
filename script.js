a2a3753fe2c31874cf7aa20b43b7ae5dbeb493f3
Adilkhan <103318672+demicat1@users.noreply.github.com>
2022-04-14 20:54:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function open() {
-  var mySidebar = document.getElementById('mySidebar');
-  mySidebar.style.display = 'block';
-}
 
-function close() {
-  document.getElementById('mySidebar').style.display = 'none';
-}
