d2a4ec57f181231bc8c9b65aa464e13f77c473e7
Adilkhan <adilkhant19@gmail.com>
2022-04-14 18:16:57 +0600

    tested main page

---
+++ script.js
@@ -0,0 +1,8 @@
+function open() {
+  var mySidebar = document.getElementById('mySidebar');
+  mySidebar.style.display = 'block';
+}
+
+function close() {
+  document.getElementById('mySidebar').style.display = 'none';
+}
