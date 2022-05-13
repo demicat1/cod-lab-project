3a2da0194e351286464cae5a8b2c1341d473741e
chodarimon <211444@astanait.edu.kz>
2022-05-13 14:56:40 +0600

    little code cleaning

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -8,56 +8,6 @@ import 'leaflet.offline'
 import 'leaflet/dist/leaflet.css'
 import { defineComponent } from 'vue'
 
-// if (navigator.geolocation) {
-//   console.log('y')
-// } else {
-//   console.log('n')
-// }
-// navigator.geolocation.getCurrentPosition(){
-//   function(position){
-//     export default defineComponent({
-//       data() {
-//       return {
-//       map: null,
-//     }
-//   },
-//   mounted() {
-//     this.map = L.map('map', { zoomControl: false }).setView([
-//       position.coords.latitude,
-//       position.coords.longitude
-//       ],
-//       11
-//     )
-//     this.map.setMaxBounds(this.map.getBounds())
-//     L.tileLayer
-//       .offline('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-//         maximumAge: 2629800000,
-//       })
-//       .addTo(this.map)
-//   },
-// })},
-//   function(error){
-//     export default defineComponent({
-//       data() {
-//         return {
-//         map: null,
-//     }
-//   },
-//   mounted() {
-//     this.map = L.map('map', { zoomControl: false }).setView(
-//       [51.136708, 71.442031],
-//       11
-//     )
-//     this.map.setMaxBounds(this.map.getBounds())
-//     L.tileLayer
-//       .offline('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-//         maximumAge: 2629800000,
-//       })
-//       .addTo(this.map)
-//   },
-// })}
-// };
-
 export default defineComponent({
   data() {
     return {
@@ -90,13 +40,10 @@ export default defineComponent({
 
     // Markers
 
-    let markers
-
     axios.get('/carwash/getCoords').then((response) => {
-      markers = response.data
+      let markers = response.data
       markers.forEach((m) => {
-        let marker
-        marker = L.marker([m['Latitude'], m['Longitude']]).addTo(this.map)
+        let marker = L.marker([m['Latitude'], m['Longitude']]).addTo(this.map)
         marker.bindPopup(`<p>${m['Name']}</p>`)
       })
     })
