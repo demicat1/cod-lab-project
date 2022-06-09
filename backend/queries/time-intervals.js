ca29f46af8b2b2b880b4c7ddc9a6592ceb466d6d
chodarimon <211444@astanait.edu.kz>
2022-06-09 09:42:31 +0600

    excluding booked times from array

--- backend/queries/time-intervals.js
+++ backend/queries/time-intervals.js
@@ -5,11 +5,25 @@ end = '21:00'
 start = timeToNumber(start)
 end = timeToNumber(end)
 
-generateIntervals(intervals, start, end)
+data = []
+data.push(
+  '2022-04-14 01:40:00+06',
+  '2022-04-14 12:40:00+06',
+  '2022-04-16 09:00:18.114+06'
+)
+bookedTimes = data.map((x) => {
+  const date = new Date(x)
+  return new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000)
+  // 14.04.2022, 07:40:00
+  // 14.04.2022, 18:40:00
+  // 16.04.2022, 15:00:18
+})
 
-// intervals.forEach((i) => {
-//   console.log(i)
-// })
+getIntervals(start, end, bookedTimes)
+
+intervals.forEach((i) => {
+  console.log(i)
+})
 
 function timeToNumber(time) {
   time = time.split(':')
@@ -19,20 +33,31 @@ function timeToNumber(time) {
 
 function getIntervals(start, end, bookedTimes = []) {
   console.log(intervals, start, end, bookedTimes)
-  //return generateIntervals(intervals, start, end, bookedTimes);
+  return generateIntervals(intervals, start, end, bookedTimes)
 }
 
-// TODO exclude from array time slots containing in bookedTimes
-function generateIntervals(array, start, end, bookedTimes = []) {
+function generateIntervals(array = [], start, end, bookedTimes = []) {
+  isBooked = false
+
+  hours = bookedTimes.map((x) => {
+    return (x.getHours() + x.getMinutes() / 60 + x.getSeconds() / 3600).toFixed(2)
+  })
+  hours.forEach((h) => {
+    if (start <= h && (start + 0.5) >= h) {
+      isBooked = true
+      return
+    }
+  })
   interval = []
   interval.push(start)
   interval.push((start += 0.5))
   interval = intervalToString(interval)
 
-  array.push(interval)
+  if(!isBooked)
+    array.push(interval)
 
   if (start >= end) return array
-  generateIntervals(array, start, end)
+  generateIntervals(array, start, end, bookedTimes)
 }
 
 function intervalToString(interval) {
@@ -51,11 +76,10 @@ function formatTime(hourMark) {
 
   if (Number.isInteger(hourMark)) {
     string = hourMark.toString() + minute
-  }
-  else {
+  } else {
     minute = hourMark % Math.floor(hourMark)
     minute *= 60
-    minute = ":" + Math.round(minute).toString()
+    minute = ':' + Math.round(minute).toString()
     string = Math.floor(hourMark).toString() + minute
   }
 
