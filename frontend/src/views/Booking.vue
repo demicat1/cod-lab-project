0bdf9af2a47858cb70e9688e5d02e26529030085
chodarimon <211444@astanait.edu.kz>
2022-06-12 16:31:15 +0600

    services front

--- frontend/src/views/Booking.vue
+++ frontend/src/views/Booking.vue
@@ -3,11 +3,47 @@
     <h1 class="title">{{ service.Name }}</h1>
     <p class="rating">{{ service.Address }}</p>
     <p class="adress">Rating: {{ service.Rating }}</p>
+    <h2>Choose an услугу:</h2>
     <div class="service-list">
-      <div class="service-item" v-for="serv in servs">
-        <p>{{ serv.Name }}</p>
-        <p>{{ serv.Description }}</p>
-        <p>{{ serv.Price }}</p>
+      <div class="service-item">
+        <p><b>Кузов+салон</b></p>
+        <p>Полная мойка авто снаружи с помощью чистящих средств, сушка машины</p>
+        <p class="price">5 000</p>
+      </div>
+      <div class="service-item">
+        <p><b>Lorem, ipsum.</b></p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi?</p>
+        <p class="price">6 000</p>
+      </div>
+      <div class="service-item">
+        <p><b>Lorem.</b></p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, doloribus.</p>
+        <p class="price">7 000</p>
+      </div>
+      <div class="service-item">
+        <p><b>Nostrum?</b></p>
+        <p>Quae exercitationem officiis et qui omnis! Ipsa error aperiam ut!</p>
+        <p class="price">8 000</p>
+      </div>
+      <div class="service-item">
+        <p><b>Earum.</b></p>
+        <p>Ad, sint. At, vero sunt illum corporis amet ea sint.</p>
+        <p class="price">9 000</p>
+      </div>
+      <div class="service-item">
+        <p><b>Distinctio.</b></p>
+        <p>Doloribus quo harum quisquam expedita neque ullam. Fugit, ullam possimus!</p>
+        <p class="price">10 000</p>
+      </div>
+      <div class="service-item">
+        <p><b>Blanditiis!</b></p>
+        <p>Optio, ad incidunt! Dolor nobis quas ipsam at, vero id.</p>
+        <p class="price">11 000</p>
+      </div>
+      <div class="service-item">
+        <p><b>Maiores!</b></p>
+        <p>Non enim ab soluta voluptates, maxime reprehenderit ipsa sint minima?</p>
+        <p class="price">12 000</p>
       </div>
     </div>
     <input type="date" id="date" class="date" v-model="date" />
@@ -59,6 +95,7 @@ await getServiceList()
 </script>
 
 <style scoped>
+
 button {
   border: 1px solid #000;
 }
@@ -66,17 +103,47 @@ button {
 .booking-container {
   display: flex;
   flex-direction: column;
-  min-height: 400px;
 }
 
 .booking-container > * {
   margin: 0.5rem;
 }
+
+.service-list {
+  overflow-y: auto;
+  max-height: 50vh;
+  margin: 1rem 0;
+  padding: .5em;
+  padding-left: 0;
+  border-block: 2px solid var(--primary);
+}
+
+.service-item {
+  border: 2px solid var(--primary);
+  text-align: start;
+  border-radius: 1em;
+  padding: .5em;
+  margin-top: .5em;
+  transition: 0.3s;
+}
+
+.service-item:hover{
+  background-color: var(--primary);
+  border: 2px solid var(--secondary);
+}
+
+.service-item p:not(:nth-child(1)){
+  margin-top: .5em;
+}
+
+.price {
+  text-align: right;
+}
 .date {
   background-color: var(--primary);
   border: 1px solid #000;
   border-radius: 0.5em;
-  padding: 0.3em 1em;
+  padding: .3em 1em;
   align-self: center;
   transition: 0.3s;
 }
@@ -104,5 +171,24 @@ button {
 
 .accent:hover {
   background-color: var(--accent-dark);
+}
+
+/* Track */
+::-webkit-scrollbar {
+  width: 13px;
+}
+
+::-webkit-scrollbar-track {
+  border-radius: 10px;
+  background-color: var(--primary);
+}
+
+::-webkit-scrollbar-thumb {
+  background: var(--secondary);
+  border-radius: 7px;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: var(--secondary-dark);
 }
 </style>
